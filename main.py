d56a2223408c7968d359fe23f6fbe307bb593ed0
MMatallana <40077810+MMatallana@users.noreply.github.com>
2018-06-16 12:17:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-def sum(x,y):
-  suma= x + y
-  return suma
-def mul(x,y):
-  multiplicacion=x*y
-  return multiplicacion
-def poten(x,y):
-  potencia = x^y
-  return potencia
+p = input(str("¿Qué tipo de operación desea realizar?, use (suma, resta, multiplicacion, division o potencia) para definirla:"))
+x = int(input("Ingrese primer número:",))
+y = int(input("Ingrese segundo número:",))
+
+if p=="suma":
+  print("La suma es: ", x+y)
+elif p=="multiplicacion":
+  print("La multiplicacion es: ", x*y)
+elif p=="potencia":
+  print("La potencia es: ",x**y)
+elif p=="resta":
+  print("La resta es: ",x-y)
+elif p=="division":
+  print("La división es: ", x/y)
+else:
+  print("Aún no creamos esa variable")
